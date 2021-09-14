144fafd6f1d483e111572a25efc80f8cbcd1d471
Martin Isaksen <izaxen@telia.com>
2021-09-14 20:54:34 +0200

    Setup user and password class

---
+++ backend/src/main/java/models/User.java
@@ -0,0 +1,99 @@
+package models;
+
+import nosqlite.annotations.Id;
+
+import java.util.Date;
+
+public class User {
+    @Id
+    private int ID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int funds;
+    private String password;
+    private Date date_created;
+
+    public User(){
+
+    }
+
+    public User(int ID, String firstName, String lastName, String email, int funds, String password, Date date_created) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.funds = funds;
+        this.password = password;
+        this.date_created = date_created;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "ID=" + ID +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", funds=" + funds +
+                ", password='" + password + '\'' +
+                ", date_created=" + date_created +
+                '}';
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getFunds() {
+        return funds;
+    }
+
+    public void setFunds(int funds) {
+        this.funds = funds;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+}
--- backend/src/main/java/utils/Application.java
+++ backend/src/main/java/utils/Application.java
@@ -1,8 +1,6 @@
 package utils;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Application {
     Connection con;
@@ -14,6 +12,7 @@ public class Application {
         }
         else{
             new Routes();
+            doExampleQuery();
         }
     }
 
@@ -26,6 +25,35 @@ public class Application {
             } catch (SQLException e) {
                 e.printStackTrace();
             }
+        }
+    }
+
+    private void doExampleQuery() { //TODO erase this one
+
+        try {
+            int userID = 1;
+            // Good habit: Never use non-prepared statement
+            // Statement stmt = con.createStatement();
+            // SQL-injection possible with the query below since we're concatenating strings
+            // ResultSet rs=stmt.executeQuery("SELECT * FROM emp WHERE emp.id = " + userID);
+            PreparedStatement pStatement = con.prepareStatement("SELECT ID, first_name, date_created FROM user WHERE id = ?");
+            pStatement.setInt(1, userID);
+            ResultSet rs = pStatement.executeQuery();
+
+            while(rs.next()) {
+                // We must manually specify at which index and which datatypes each column in the result is.
+                System.out.println(
+                        rs.getInt(1)
+                                + "  " +
+                                rs.getString(2)
+                                + "  " +
+                                rs.getTimestamp(3)
+                );
+            }
+            con.close();
+        }catch(Exception e){
+
+            System.out.println(e);
         }
     }
 }
