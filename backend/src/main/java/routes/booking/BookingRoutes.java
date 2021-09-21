a131a77a5b5dd1cb67bbdd4f4b4eb26cf51aa04e
udden89 <77149458+udden89@users.noreply.github.com>
2021-09-21 16:29:38 +0200

    Update BookingRoutes.java

--- backend/src/main/java/routes/booking/BookingRoutes.java
+++ backend/src/main/java/routes/booking/BookingRoutes.java
@@ -1,20 +1,26 @@
 package routes.booking;
 
+import application.booking.BookingLogic;
 import express.Express;
+import models.Booking;
 import repositories.BookingRepository;
 
 public class BookingRoutes {
 
     BookingRepository bookingRepository;
+    BookingLogic bookingLogic;
 
     public BookingRoutes(Express app, BookingRepository bookingRepository) {
         createBookingRoute(app);
         this.bookingRepository = bookingRepository;
+        this.bookingLogic = new BookingLogic(bookingRepository);
     }
 
     private void createBookingRoute(Express app){
         app.post("/api/createBooking", ((req, res) -> {
-            System.out.println(req.body());
+            Booking newBooking = req.body(Booking.class);
+            newBooking = bookingLogic.createAndGetConfirmationBooking(newBooking);
+            System.out.println(newBooking);
         }));
     }
 }
