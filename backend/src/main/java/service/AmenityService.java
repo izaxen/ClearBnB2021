ce12bcd518b3599c55a5f6e9e14f2b0daccbe6a6
Yang Li <odielee@hotmail.com>
2021-09-23 14:10:43 +0200

    fix order problem
    Co-Authored-By: Martin Isaksen <77095450+izaxen@users.noreply.github.com>
    Co-Authored-By: udden89 <77149458+udden89@users.noreply.github.com>

--- backend/src/main/java/service/AmenityService.java
+++ backend/src/main/java/service/AmenityService.java
@@ -8,10 +8,7 @@ public class AmenityService {
 
 
     public Amenities convertAddAmenitiesToAmenities(AddAmenityDTO add, Listing listing){
-        return new Amenities(listing, add.isBathTub(), add.isBubblePool(), add.isCycle(), add.isDoubleBed(),
-        add.isSauna(), add.isStove(), add.isParkingLot());
-
-
-
+        return new Amenities(listing, add.isBathTub(), add.isParkingLot(), add.isStove(), add.isDoubleBed(),
+        add.isBubblePool(), add.isCycle(), add.isSauna());
     }
 }
