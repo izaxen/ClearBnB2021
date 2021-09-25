0b9c85ffc7d590f031209c14a6a537992848a651
Yang Li <odielee@hotmail.com>
2021-09-25 08:02:40 +0200

    filter for availableDate logic now works correctly
    frontend component needed

--- backend/src/main/java/application/ListingLogic.java
+++ backend/src/main/java/application/ListingLogic.java
@@ -37,21 +37,18 @@ public class ListingLogic {
         // Filter does not run condition?
         // If wrong name on filter, system crashes, which means we have connected to our filter
         // But why it oe
-        Filter availableDateFilter = session.enableFilter("dateFilter");
-        System.out.println(session.getEnabledFilter("dateFilter"));
+//        Filter availableDateFilter = session.enableFilter("dateFilter");
+//        System.out.println(session.getEnabledFilter("dateFilter"));
 
         Timestamp ts1 = listing.getAvailableStartDate();
         Timestamp ts2 = listing.getAvailableEndDate();
 
-//        ts1 = new Timestamp(163239371);
-
-        availableDateFilter.setParameter("availableStartDate", ts1);
-        availableDateFilter.setParameter("availableEndDate", ts2);
+//        availableDateFilter.setParameter("availableStartDate", ts1);
+//        availableDateFilter.setParameter("availableEndDate", ts2);
 
         System.out.println("ts1: " + ts1);
         System.out.println("ts2: " + ts2);
 
-//        List<Listing> matchedListing = listingRepository.findAllListings();
 
 
         List<Listing> matchedListing = session.createQuery("FROM Listing as l WHERE l.availableStartDate <= :selectedStartDate and l.availableEndDate >= :selectedEndDate", Listing.class)
@@ -59,7 +56,12 @@ public class ListingLogic {
                 .setParameter("selectedEndDate", ts2)
                 .list();
 
-        System.out.println("matchedLsiting: " + matchedListing);
+        // for testing
+//        for (Listing l : matchedListing
+//             ) {
+//            System.out.println( l);
+//
+//        }
 
         return matchedListing;
     }
